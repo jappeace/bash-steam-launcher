6bba0eeecb5ec39f3a2195b427feba284e8b99bd
Jappie Klooster <jappieklooster@hotmail.com>
2013-12-16 21:53:03 +0100

    script now runs, by messing around a bit with permisions

--- steam.sh
+++ steam.sh
@@ -5,15 +5,35 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 #the steam user
-$steam="steam"
+steam="steam"
 #the user that has acces to the x server
-$user="jappie"
-$steamHome="/home/"$steam
-$userHome="/home/"$user
-$userFile=$userHome/"display"
-$steamFile=$steamHome/"display"
+user="jappie"
+steamHome="/home/"$steam
+userHome="/home/"$user
+userFile=$userHome/"display"
+steamFile=$steamHome/"display"
+xauth=".Xauthority"
+
+echo "deleting old files"
 rm $origFile $steamFile
-sudo -u $user xauth nextract $userFile :0
-sudo mv $userFile $steamHome/
-sudo chown $steam:$steam $steamFile
-sudo -u $steam nmerge $steamFile
+rm $steamHome"/.Xauthority"
+
+echo "extracting magic cookie"
+sudo -u $user xauth nextract $userFile ":0"
+
+echo "moving the cookie to the steam directory"
+mv $userFile $steamHome/
+
+echo "fixing permisions"
+chown $steam:$steam $steamFile
+chmod 777 $steamFile
+chmod 664 $userHome"/"$xauth
+chmod -R 771 "/run/gdm"
+
+echo "importing the cookie"
+(
+	sudo -u $steam xauth -i nmerge $steamFile
+)
+sudo -u $steam bash $steamHome"/steam/steam.sh"
+echo "cleaning up"
+#rm $origFile $steamFile
