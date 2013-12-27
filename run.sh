1c03e38da7f7cc1b4b0a6625cd3725a211980110
Jappie Klooster <jappieklooster@hotmail.com>
2013-12-27 19:40:04 +0000

    added a sort of auto installer, it symlinks the right executable in a bin directory

--- run.sh
+++ run.sh
@@ -8,12 +8,17 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 RUNFILE=$DIR"/bootstrap.sh"
-PERM=`sudo -l | grep $RUNFILE`
+EXEC="/usr/local/bin/runsteam"
+PERM=`sudo -l | grep $EXEC`
 echo $PERM
 if [$PERM == ""] 
 then
-	echo "adding " $RUNFILE " to run as root without password, ctrl+c to cancel"
-	echo "the command: sudo sh -c '%wheel ALL=(root) NOPASSWD: '$RUNFILE >> /etc/sudoers"
-	sudo sh -c "%wheel ALL=(root) NOPASSWD: "$RUNFILE >> /etc/sudoers
+	sudo ln -s $DIR"/run.sh" $EXEC
+	sudo chmod +x $EXEC
+	echo "please add the following line to the opening editor:"
+	echo "%wheel ALL=(root) NOPASSWD: $EXEC"
+	sudo visudo
+	echo "now rerun this script"
+else
+	sudo bash $RUNFILE
 fi
-sudo bash $RUNFILE
