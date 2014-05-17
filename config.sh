da1209830615b68b10330a9e3ca70c298d3d9ff3
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-17 17:11:51 +0200

    deleted previous work

--- config.sh
+++
@@ -1,9 +0,0 @@
-#the steam user
-steam="steam"
-#the user that has acces to the x server
-user="jappie"
-steamHome="/home/"$steam
-userHome="/home/"$user
-userFile=$userHome/"display"
-steamFile=$steamHome/"display"
-xauth=".Xauthority"
--- openwindow.sh
+++
@@ -1,7 +0,0 @@
-source $1
-
-# run bumblebee
-sudo bumblebeed --daemon
-
-# run steam with bumblebee, primusrun
-sudo -u $steam optirun $steamHome"/steam/steam.sh"
